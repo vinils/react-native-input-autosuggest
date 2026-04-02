fe680c1d1afbbeb9308f61c6a6c428556c24de23
VLS <vini_ls@hotmail.com>
2026-04-02 14:11:55 -0300

    aaaa

--- example/App.tsx
+++ example/App.tsx
@@ -5,6 +5,7 @@
  * @format
  */
 
+import React from 'react';
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 import {
