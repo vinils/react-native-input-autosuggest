fd41af8a7ff22509cb80f1640df34cfd5765cb15
VLS <vini_ls@hotmail.com>
2026-04-13 14:19:47 -0300

    adding react native web

--- example/index.js
+++ example/index.js
@@ -4,6 +4,19 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import appJson from './app.json';
 
+const appName = appJson.name;
 AppRegistry.registerComponent(appName, () => App);
+
+if (typeof document !== 'undefined') {
+  const rootTag = document.getElementById('root') || document.createElement('div');
+
+  if (!rootTag.parentElement) {
+    document.body.appendChild(rootTag);
+  }
+
+  AppRegistry.runApplication(appName, {
+    rootTag,
+  });
+}
