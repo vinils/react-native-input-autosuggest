ad3cdf9a3bdb3ace57ce6d2b07f1917c706cc231
VLS <vini_ls@hotmail.com>
2026-05-15 18:01:46 -0300

    testbundle25

--- example/metro.config.js
+++ example/metro.config.js
@@ -2,9 +2,9 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://github.io
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
 const config = {};
 
